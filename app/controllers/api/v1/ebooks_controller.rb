42b1f8c0e66a5f196a3cb9f1d735cbd017fd1cf2
Govind Birajdar <govindbirajdar888@gmail.com>
2026-07-10 14:35:47 +0530

    done with create and listing ebook api implementation

--- app/controllers/api/v1/ebooks_controller.rb
+++ app/controllers/api/v1/ebooks_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class EbooksController < ApplicationController
-
       def index
         ebooks = Ebook.order(created_at: :desc)
 
@@ -13,7 +12,32 @@ module Api
         )
       end
 
-      
+      def create
+        ebook = Ebook.new(ebook_params)
+
+        if ebook.save
+          success_response(
+            ebook.as_json(only: %i[id title author created_at]),
+            "Ebook uploaded successfully",
+            :created
+          )
+        else
+          error_response(
+            "Validation failed",
+            ebook.errors.full_messages
+          )
+        end
+      end
+
+
+      private
+
+      def ebook_params
+        params.permit(:title,:author,:file,:cover)
+      end
+
     end
   end
+
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,13 @@
 class ApplicationController < ActionController::API
+	include Rails.application.routes.url_helpers
+
   private
 
   def success_response(data = nil, message = "Success", status = :ok)
-    render json: {
-      success: true,
-      message: message,
-      data: data
-    }, status: status
+    render json: { success: true, message: message, data: data }, status: status
   end
 
   def error_response(message = "Something went wrong", errors = [], status = :unprocessable_entity)
-    render json: {
-      success: false,
-      message: message,
-      errors: errors
-    }, status: status
+    render json: { success: false, message: message, errors: errors }, status: status
   end
 end
